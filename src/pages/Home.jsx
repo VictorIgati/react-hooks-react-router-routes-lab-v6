edcdf99d5d71535a19eec99b0a58975e7d3dbca6
VictorIgati <victor.igati@student.moringaschool.com>
2025-01-23 14:24:08 +0300

    worked with the derivables required

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react";
+// src/pages/Home.jsx
+import { useState, useEffect } from 'react';
+import NavBar from '../components/NavBar';
+import MovieCard from '../components/MovieCard';
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/movies')
+      .then(r => r.json())
+      .then(moviesData => setMovies(moviesData));
+  }, []);
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Home Page</h1>
+      <div className="movie-list">
+        {movies.map(movie => (
+          <MovieCard 
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+          />
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,34 @@
-import { useEffect, useState } from "react";
+// src/pages/Movie.jsx
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 function Movie() {
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then(r => r.json())
+      .then(movieData => setMovie(movieData));
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time} minutes</p>
+      <div>
+        {movie.genres.map((genre, index) => (
+          <span key={index} style={{ marginRight: '10px' }}>
+            {genre}
+          </span>
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Movie;
